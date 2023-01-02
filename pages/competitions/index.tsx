ca30999de96cb0ca60200b75a517ef2516fa1239
pandora-1 <michaelkahari.mk@gmail.com>
2023-01-02 23:13:47 +0700

    integrate competitions

--- pages/competitions/index.tsx
+++ pages/competitions/index.tsx
@@ -6,10 +6,28 @@ import Navbar from "@/components/navbar";
 import { useWindowSize } from "@/utils/windowsize";
 import type { NextPage } from "next";
 import Image from "next/image";
+import {useQuery} from 'react-query';
+import {useState} from "react";
 
 const ListCompetition: NextPage = () => {
   const size = useWindowSize();
   const windowSize = size.width;
+
+  const [page, setPage] = useState(1);
+
+  const getDataNews = async (hal) => {
+    console.log(hal)
+		const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + "/competition/?page=" + hal);
+		return res.json();
+	};
+	// Using the hook
+	const {data, error, isLoading} = useQuery({
+    queryKey: ['projects', page],
+    queryFn: () => getDataNews(page),
+    keepPreviousData : true
+  })
+
+  console.log(data)
 
   return (
     <div className="flex min-h-screen flex-col">
@@ -80,43 +98,44 @@ const ListCompetition: NextPage = () => {
       </form>
 
       <div className={`grid grid-cols-4 gap-2 mt-12 mb-9 lg:mx-36 mx-7`}>
-        <div className={`lg:col-span-1 lg:mx-0 col-span-2 mx-auto`}>
-          <EventCard
-            link="/competitions/id"
-            title="Event News"
-            date="DD/MM/YYYY"
+        {data && data.data.competitions.map((d, i) => {
+          return(
+            <div key={i} className={`lg:col-span-1 lg:mx-0 col-span-2 mx-auto`}>
+            <EventCard
+            link={d.url}
+            title={d.name}
+            date={d.date.slice(0,10)}
             views="9999"
-            image="pp"
+            image={d.photoURLs[0].url}
           />
-        </div>
-        <div className={`lg:col-span-1 lg:mx-0 col-span-2 mx-auto`}>
-          <EventCard
-            link="/competitions/id"
-            title="Event News"
-            date="DD/MM/YYYY"
-            views="9999"
-            image="pp"
-          />
-        </div>
-        <div className={`lg:col-span-1 lg:mx-0 col-span-2 mx-auto`}>
-          <EventCard
-            link="/competitions/id"
-            title="Event News"
-            date="DD/MM/YYYY"
-            views="9999"
-            image="pp"
-          />
-        </div>
-        <div className={`lg:col-span-1 lg:mx-0 col-span-2 mx-auto`}>
-          <EventCard
-            link="/competitions/id"
-            title="Event News"
-            date="DD/MM/YYYY"
-            views="9999"
-            image="pp"
-          />
-        </div>
+          </div>
+          )
+        })}
       </div>
+
+      <nav aria-label="Page navigation example" className="m-auto mb-8">
+        <ul className="inline-flex items-center -space-x-px">
+            <li onClick={() => setPage(Math.max(page - 1,0))}>
+                <a href="#" className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <span className="sr-only">Previous</span>
+                  <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                </a>
+              </li>
+          {data && Array.from(Array(data.data.pageTotal), (e, i) => {
+            return(
+              <li key={i}>
+              <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{i + 1}</a>
+            </li>
+            )
+          })}
+          <li onClick={() => setPage(Math.min(page + 1,data.data.pageTotal))}>
+            <a className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+              <span className="sr-only">Next</span>
+              <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
       <Footer />
     </div>
   );
