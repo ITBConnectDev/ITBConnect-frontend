17ea7d0bdbfa5fc0175aafe852efaeb0f5b2f8f4
arsaizdihar <arsadihar@gmail.com>
2023-01-21 22:52:02 +0700

    Fix email validation

--- utils/string.ts
+++ utils/string.ts
@@ -1,7 +1,6 @@
 export function validateEmail(email: string): boolean {
   // Regular expression to validate email
-  const emailRegex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailRegex = /^[1-3][0-9]{7}@mahasiswa\.itb\.ac\.id$/g;
 
   // Return true if email is valid, false otherwise
   return emailRegex.test(String(email).toLowerCase());
